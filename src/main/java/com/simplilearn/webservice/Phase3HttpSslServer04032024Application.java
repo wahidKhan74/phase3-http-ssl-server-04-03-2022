7300ff75873d9f657d01819599266fb1be1fcbac
Wahid Khan <wahid.khan74@gmail.com>
2022-04-03 16:30:53 +0000

    added ssl self signed cirtificate

---
+++ src/main/java/com/simplilearn/webservice/Phase3HttpSslServer04032024Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3HttpSslServer04032024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3HttpSslServer04032024Application.class, args);
+	}
+
+}
